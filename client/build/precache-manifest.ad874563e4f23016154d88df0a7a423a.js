4a882291841c734768b3f1647d20dff93e3a6421
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-09-17 20:37:25 -0400

    add build folder to git, against all things good in the world

---
+++ client/build/precache-manifest.ad874563e4f23016154d88df0a7a423a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f5b9669dbcec8a9b17093e7cc0fd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "304a42d013e17a9056f0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "29bddece52509b848ad1",
+    "url": "/static/js/2.fe34d606.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe34d606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304a42d013e17a9056f0",
+    "url": "/static/js/main.37e7df14.chunk.js"
+  },
+  {
+    "revision": "632f94492b85b1d10e3e",
+    "url": "/static/js/runtime-main.e2f327b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
