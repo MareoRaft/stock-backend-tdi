aab1766406fbc22f6188d88bde3990a29e90bfef
Matthew Lancellotti <mvlancellotti@gmail.com>
2020-09-18 09:43:30 -0400

    get superstitious

--- server/main.py
+++ server/main.py
@@ -36,9 +36,6 @@ def index():
 # Main execution loop
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-	app.run(
-		host='0.0.0.0',
-		port=port,
-	)
+	app.run(host='0.0.0.0', port=port)
 
 
